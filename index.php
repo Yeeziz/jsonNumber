4993f4e352cd5e70f68b470823cdce323022317b
Yeeziz <giannuzzisebastiano@gmail.com>
2022-02-01 07:20:40 +0000

    ahah funny ahah lol

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-
+<?php
+    $fileData=json_decode(file_get_contents("dati.json"));
+    foreach($fileData as $indice => $valore){
+        $d = $valore -> dato;
+        echo "[$indice] $d<br>";
+    }
+    $newValore = new stdClass();
+    $newValore -> dato = ($d+1);
+    $fileData[] = $newValore;
+    file_put_contents("dati.json", json_encode($fileData));
+?>
